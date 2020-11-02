edf492ea0246a1e7d8aac9274cd8fd4c318fe914
hiroshi yamato <yamato@gmail.com>
2020-11-02 14:59:57 +0900

    Initial Commit

---
+++ LinkTestSwift/LinkTestSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkTestSwiftApp.swift
+//  LinkTestSwift
+//
+//  Created by hiroshi yamato on 2020/11/02.
+//
+
+import SwiftUI
+
+@main
+struct LinkTestSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
